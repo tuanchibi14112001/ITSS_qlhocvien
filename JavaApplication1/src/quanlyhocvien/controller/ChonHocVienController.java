cddd2699762b3fff4108ba23b3864ec2486c0f63
NangTranXuan <nang.tx194633@sis.hust.edu.vn>
2023-02-08 17:31:10 +0700

    them hoc vien vao lop hoc

--- JavaApplication1/src/quanlyhocvien/controller/ChonHocVienController.java
+++ JavaApplication1/src/quanlyhocvien/controller/ChonHocVienController.java
@@ -14,6 +14,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -35,12 +37,16 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import quanlyhocvien.model.HocVien;
+import quanlyhocvien.model.HocVienLopHoc;
+import quanlyhocvien.model.KhoaHoc;
 import quanlyhocvien.service.HocVienService;
 import quanlyhocvien.service.HocVienServiceImpl;
 import quanlyhocvien.service.LopHocChiTietService;
 import quanlyhocvien.service.LopHocChiTietServiceImpl;
 import quanlyhocvien.utility.ClassTableModel;
 import quanlyhocvien.view.HocVienInfoJFrame;
+import quanlyhocvien.view.KhoaHocInfoJFrame;
+import quanlyhocvien.view.ThemThongTinHocVienJFrame;
 
 /**
  *
@@ -53,6 +59,7 @@ public class ChonHocVienController {
     private TableRowSorter<TableModel> rowSorter = null;//sap xep hang
     private HocVienService hoc_vien_service = null;
     private LopHocChiTietService lop_hoc_chi_tiet_service = null;
+    private LopHocChiTietService lop_hoc_chi_tiet_service2 = null;
     private String[] listColumn = {"ID", "STT", "Họ tên", "Giới tính", "Ngày sinh", "Email", "Số điện thoại"};
 
     public ChonHocVienController() {
@@ -64,6 +71,7 @@ public class ChonHocVienController {
         this.jtf_search = jtf_search;
         this.hoc_vien_service = new HocVienServiceImpl();
         this.lop_hoc_chi_tiet_service = new LopHocChiTietServiceImpl();
+        this.lop_hoc_chi_tiet_service2 = new LopHocChiTietServiceImpl();
         this.hoc_vien_service = new HocVienServiceImpl();
     }
 
@@ -116,6 +124,43 @@ public class ChonHocVienController {
             @Override
             public void changedUpdate(DocumentEvent de) {
             }
+        });
+        
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() > 1 && table.getSelectedRow() != -1) {//click 2 lan va co hang trong bang
+                    DefaultTableModel model = (DefaultTableModel) table.getModel();
+                    int selectedRowIndex = table.getSelectedRow();
+                    selectedRowIndex = table.convertRowIndexToModel(selectedRowIndex);
+                    //System.out.println(selectedRowIndex);
+                    int ma_hoc_vien = (int) model.getValueAt(selectedRowIndex, 0);
+                    SimpleDateFormat formattter = new SimpleDateFormat("dd-MM-yyyy");
+                    Date date = new Date(System.currentTimeMillis());
+                    
+
+                    HocVienLopHoc hocVienLopHoc = new HocVienLopHoc();
+                    hocVienLopHoc.setMa_hoc_vien(ma_hoc_vien);
+                    hocVienLopHoc.setNgay_dang_ky(date);
+                    hocVienLopHoc.setThanh_toan(true);
+                    hocVienLopHoc.setTinh_trang(true);
+                    
+                    HocVien hocVien = new HocVien();
+                    hocVien = hoc_vien_service.getHocVienID(ma_hoc_vien);
+                    
+                    System.out.println(hocVien.getHo_ten());
+                    
+
+                    //hoc_vien.setHo_ten(model.getValueAt(selectedRowIndex, 2).toString());
+                    ThemThongTinHocVienJFrame frame = new ThemThongTinHocVienJFrame(ma_lop_hoc,hocVienLopHoc, hocVien);
+                    frame.setTitle("Thông tin chi tiết");
+                    frame.setResizable(false);
+                    frame.setLocationRelativeTo(null);
+                    frame.setVisible(true);
+
+                }
+            }
+
         });
         
 
